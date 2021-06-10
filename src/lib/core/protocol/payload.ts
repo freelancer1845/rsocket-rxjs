585facb18fb746e29abdbca3022b59f08963c9b8
Jascha Riedel <jaschelite@googlemail.com>
2021-06-10 18:05:45 +0200

    Payload data cannot be undefined anymore
    Defaults to new Uint8Array(0)
    
    Signed-off-by: Jascha Riedel <jaschelite@googlemail.com>

--- src/lib/core/protocol/payload.ts
+++ src/lib/core/protocol/payload.ts
@@ -6,6 +6,12 @@ export class Payload {
 
 
     constructor(public readonly data: Uint8Array, public readonly metadata: Uint8Array = new Uint8Array(0)) {
+        if (this.data == undefined) {
+            this.data = new Uint8Array(0);
+        }
+        if (this.metadata == undefined) {
+            this.metadata == new Uint8Array(0);
+        }
     }
 
     public hasMetadata(): boolean {
