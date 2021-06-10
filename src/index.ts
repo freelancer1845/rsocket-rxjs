8be78579df5c9ad590ffaee0a2cf063dfc9651cd
Jascha Riedel <jaschelite@googlemail.com>
2021-06-10 13:05:29 +0200

    Export more
    Signed-off-by: Jascha Riedel <jaschelite@googlemail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,18 @@
  * Public API of rsocket-rxjs
  */
 
-export { RSocketRoutingResponder } from './lib/extensions/messages/rsocket-routing-responder';
+export { WebsocketTransport } from './lib/core/transport/websocket-transport.impl';
 
 export { SpringRSocketMessagingBuilder } from './lib/api/rsocket-factory';
 
-
-export { MessageRoutingRSocket } from './lib/extensions/messages/message-routing-rsocket';
-export { RSocketClient } from './lib/core/rsocket-client.impl';
 export { RSocketResponder, RSocket } from './lib/api/rsocket.api';
+export { Payload } from './lib/core/protocol/payload';
+export { EncodingRSocketResponder } from './lib/extensions/encoding-rsocket-responder';
+export { RSocketRoutingResponder } from './lib/extensions/messages/rsocket-routing-responder';
+
+export { RSocketClient } from './lib/core/rsocket-client.impl';
+export { EncodingRSocket, DecodedPayload, RSocketEncoderRequestOptions, RSocketDecoder, RSocketEncoder } from './lib/extensions/encoding-rsocket-client';
+export { MessageRoutingRSocket, RoutedPayload, MessageRoutingOptions } from './lib/extensions/messages/message-routing-rsocket';
+
 export { RSocketConfig } from './lib/core/config/rsocket-config';
-export { WebsocketTransport } from './lib/core/transport/websocket-transport.impl';
-export { FluentRequest } from './lib/extensions/messages/rsocket-fluent';
+export { WellKnownMimeTypes } from './lib/extensions/well-known-mime-types';
